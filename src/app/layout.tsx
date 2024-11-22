84958a854fda38187959beb8a95f892f1595bfab
rickmoralis <94112399+rickmoralis@users.noreply.github.com>
2024-11-22 03:48:30 +0300

    fix layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,62 +24,6 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body className={sans_caption.className}>
-              <GoogleAnalytics gaId="8008797785" />
-            <GoogleTagManager gtmId={"G-D9NWJ5P27W"} />
-            <Script 
-               id="gtag-js" 
-               strategy="afterInteractive" 
-               src="https://www.googletagmanager.com/gtag/js?id=G-D9NWJ5P27W"
-            />
-            <Script strategy="afterInteractive">
-               {`
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', 'G-D9NWJ5P27W');
-               `}
-            </Script>
-        {/* Яндекс Метрика */}
-        <Script id="yandex-metrika" strategy="afterInteractive">
-            {`(function(m,e,t,r,i,k,a){
-                        m[i]=m[i]||function(){
-                            (m[i].a=m[i].a||[]).push(arguments)
-                        }; 
-                        m[i].l=1*new Date(); 
-                        k=e.createElement(t),a=e.getElementsByTagName(t)[0],
-                        k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-                    })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-                    
-                    ym(97120978, "init", {
-                        defer: true,
-                        clickmap: true,
-                        trackLinks: true,
-                        accurateTrackBounce: true,
-                        webvisor: true
-                    });`}
-        </Script>
-        <noscript>
-            <div>
-                <img src="https://mc.yandex.ru/watch/97120978" style={{ position: "absolute", left: "-9999px" }} alt="ya_metrika" />
-            </div>
-        </noscript>
-
-        {/* Data Traffic Guard */}
-        <Script>
-            {`var dataTrafficGuard = dataTrafficGuard || [];
-                    dataTrafficGuard.push(['property', 'tg-013364-001']);
-                    dataTrafficGuard.push(['event', 'pageview']);
-                    (function() {
-                        var tg = document.createElement('script'); 
-                        tg.type = 'text/javascript'; 
-                        tg.async = true; 
-                        tg.src = '//tgtag.io/tg.js?pid=tg-013364-001';
-                        var s = document.getElementsByTagName('script')[0];
-                        s.parentNode.insertBefore(tg, s);
-                    })();`}
-        </Script>
-
-        {/* Основной контент приложения */}
         <ContainerProvider>
             <StickyMenu />
             {children}
